b03565041578409dd9ccba4638111cbc7ac3a3fb
Melih Akay <melih.akay@metu.edu.tr>
2025-04-24 15:06:00 +0300

    Quartz sync: Apr 24, 2025, 3:06 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    // Component.Explorer(),
+    Component.Explorer(),
     Component.DesktopOnly(Component.RecentNotes(
       { title: "Recent writing" }
     ))
